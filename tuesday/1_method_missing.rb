c74b89aae2a3dc777bc7ea7e25dc449d235c5e12
Matthew Benitez <matthew.benitez@appfolio.com>
2021-11-16 12:12:38 -0800

    Add unrefactored base code for method_missing approach

--- tuesday/1_method_missing.rb
+++ tuesday/1_method_missing.rb
@@ -42,4 +42,38 @@ icecream.flavor
 # method_missing chops off the "=" at the end to get the attribute
 # name and stores its value. If the name of the called method 
 # doesn't match any of these cases, then method_missing just 
-# returns a default valuie.
+# returns a default value.
+# ------------------------------------------
+
+class Computer
+  def initialize(computer_id, data_source)
+    @id = computer_id
+    @data_source = data_sources
+  end
+
+  def mouse
+    info = @data_source.get_mouse_info(@id)
+    price = @data_source.get_mouse_price(@id)
+    result = "Mouse: #{info} ($#{price})"
+    return "* #{result}" if price >= 100
+    result
+  end
+
+  def cpu
+    info = @data_source.get_cpu_info(@id)
+    price = @data_source.get_cpu_price(@id)
+    result = "CPU: #{info} ($#{price})"
+    return "* #{result}" if price >= 100
+    result
+  end
+  
+  def keyboard
+    info = @data_source.get_cpu_info(@id)
+    price = @data_source.get_cpu_price(@id)
+    result = "CPU: #{info} ($#{price})"
+    return "* #{result}" if price >= 100
+    result
+  end
+
+  # ...
+end
