4a790b0af65e36589e1aaf902902fedcb8026132
Matthew Benitez <matthew.benitez@appfolio.com>
2021-11-16 11:43:16 -0800

    Add Dynamic Methods

--- tuesday/0_dynamic_dispatch.rb
+++ tuesday/0_dynamic_dispatch.rb
@@ -59,3 +59,26 @@ class Computer_Dynamic_Dispatch
 
   # ...
 end
+
+class Computer_Dynamic_Methods
+  def initialize(computer_id, data_source)
+    @id = computer_id
+    @data_source = data_sources
+  end
+
+  def self.define_component(name)
+    define_method(name) do
+      info = @data_source.send("get_#{name}_info", @id)
+      price = @data_source.send("get_#{name}_price", @id)
+      result = "#{name.capitalize}: #{info} ($#{price})"
+      return "* #{result}" if price >= 100
+      result
+    end
+  end
+
+  define_component :mouse
+  define_component :cpu
+  define_component :keyboard
+
+  # ...
+end
