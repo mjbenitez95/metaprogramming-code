1aede1f911ddc3ca94587e328cac2faa51621cc5
Matthew Benitez <matthew.benitez@appfolio.com>
2021-11-18 14:23:22 -0800

    Add blank slate implementation

--- tuesday/3_blank_slates.rb
+++ tuesday/3_blank_slates.rb
@@ -25,3 +25,26 @@ class ComputerWithMethodMissingBuggy
     @data_source.respond_to?("get_#{method}_info") || super
   end
 end
+
+class BlankSlateComputer < BasicObject
+  # inheriting from BasicObject (as opposed to Object, which
+  # is what classes inherently subclass) will allow our class
+  # to have a bare minimum of instance methods, providing a
+  # good starting point (or blank slate) for our implementation.
+
+  def initialize(computer_id, data_source)
+    @id = computer_id
+    @data_source = data_source
+  end
+
+  def method_missing(name)
+    super unless @data_source.respond_to?("get_#{name}_info")
+
+    info = @data_source.send("get_#{name}_info", @id)
+    price = @data_source.send("get_#{name}_price", @id)
+    result = "#{name.capitalize}: #{info} ($#{price})"
+    return "* #{result}" if price >= 100
+
+    result
+  end
+end
