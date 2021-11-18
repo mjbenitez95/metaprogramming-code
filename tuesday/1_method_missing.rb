1a9473bb3eebca3885ad6c692bba094f5a66998a
Matthew Benitez <matthew.benitez@appfolio.com>
2021-11-18 13:54:34 -0800

    Add refactored method_missing example

--- tuesday/1_method_missing.rb
+++ tuesday/1_method_missing.rb
@@ -77,3 +77,21 @@ class Computer
 
   # ...
 end
+
+class Computer
+  def initialize(computer_id, data_source)
+    @id = computer_id
+    @data_source = data_source
+  end
+
+  def method_missing(name)
+    # handles base cases
+    super if !@data_source.respond_to?("get_#{name}_info")
+
+    info = @data_source.send("get_#{name}_info", @id)
+    price = @data_source.send("get_#{name}_price", @id)
+    result = "#{name.capitalize}: #{info} ($#{price})"
+    return "* #{result}" if price >= 100
+    result
+  end
+end
