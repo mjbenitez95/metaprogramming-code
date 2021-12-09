2c47eeb891832a1988acce81e0f873fa59eece44
Matthew Benitez <matthew.benitez@appfolio.com>
2021-12-09 10:12:41 -0800

    Add example of scoping bounds inside a block

--- wednesday/0_ruby_blocks.rb
+++ wednesday/0_ruby_blocks.rb
@@ -17,3 +17,20 @@ x = "Hello"
 # the block defined below picks up the x binding above
 puts binding_example {|y| "#{x}, #{y} world!" } # => "Hello, cruel world!"
 
+def just_yield
+  yield
+end
+
+def inside_block_binding
+  top_level_variable = 1
+
+  just_yield do
+    top_level_variable += 1
+    local_to_block = 1
+  end
+
+  puts top_level_variable # => 2
+  # puts local_to_block # => error!
+end
+
+inside_block_binding
